091da0bc8014a367458dcc03e88d54f839ebe23a
Hani Ibrahim <hani.ibrahim@wimdu.com>
2016-06-23 12:30:11 +0200

    Adding dummy views

---
+++ IExpendables/MessagesExtension/CheckOutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CheckOutViewController.swift
+//  IExpendables
+//
+//  Created by Hani Ibrahim on 6/23/16.
+//  Copyright © 2016 Andres Brun. All rights reserved.
+//
+
+import UIKit
+
+class CheckOutViewController: UIViewController, SearchParameterViewController {
+    
+    var search: Search?
+}
