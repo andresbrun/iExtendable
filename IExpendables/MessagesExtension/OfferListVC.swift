a5c0410c8e20c02f8eec8f3d7afd0697df6bbafe
Andrés Brun Moreno <andres.moreno@wimdu.com>
2016-06-23 09:52:57 +0200

    Creating VC.

---
+++ IExpendables/MessagesExtension/OfferListVC.swift
@@ -0,0 +1,15 @@
+//
+//  OfferListVC.swift
+//  IExpendables
+//
+//  Created by Andres Brun on 23/06/16.
+//  Copyright © 2016 Andres Brun. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class OfferListVC: UITableViewController {
+    
+}
---
+++ IExpendables/MessagesExtension/SearchVC.swift
@@ -0,0 +1,14 @@
+//
+//  SearchVC.swift
+//  IExpendables
+//
+//  Created by Andres Brun on 23/06/16.
+//  Copyright © 2016 Andres Brun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchVC: UIViewController {
+    
+}
