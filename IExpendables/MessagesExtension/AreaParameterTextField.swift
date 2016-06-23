c7aafc66635784c5cba10c6f47de817e2d68ce0d
Hani Ibrahim <hani.ibrahim@wimdu.com>
2016-06-23 10:48:52 +0200

    Adding images

---
+++ IExpendables/MessagesExtension/AreaParameterTextField.swift
@@ -0,0 +1,16 @@
+//
+//  AreaParameterTextField.swift
+//  IExpendables
+//
+//  Created by Hani Ibrahim on 6/23/16.
+//  Copyright © 2016 Andres Brun. All rights reserved.
+//
+
+import UIKit
+
+class AreaParameterTextField: SearchParameterTextField {
+
+    override func iconImageName() -> String? {
+        return "magnifying_glass_icon"
+    }
+}
---
+++ IExpendables/MessagesExtension/GuestsParameterTextField.swift
@@ -0,0 +1,16 @@
+//
+//  GuestsParameterTextField.swift
+//  IExpendables
+//
+//  Created by Hani Ibrahim on 6/23/16.
+//  Copyright © 2016 Andres Brun. All rights reserved.
+//
+
+import UIKit
+
+class GuestsParameterTextField: SearchParameterTextField {
+
+    override func iconImageName() -> String? {
+        return "guest_icon"
+    }
+}
