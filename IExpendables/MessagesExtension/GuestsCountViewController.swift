b6c542389dc68c6022e525e050641fc4a373b57b
Andrés Brun Moreno <andres.moreno@wimdu.com>
2016-06-23 15:01:07 +0200

    Adding number of guest selector.

--- IExpendables/MessagesExtension/GuestsCountViewController.swift
+++ IExpendables/MessagesExtension/GuestsCountViewController.swift
@@ -13,3 +13,22 @@ class GuestsCountViewController: UIViewController, SearchParameterViewController
     var search: Search?
     var completion: ((Void) -> ())?
 }
+
+extension GuestsCountViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 20
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return row == 0 ? "\(row + 1) Guest" : "\(row + 1) Guests"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        search?.guests = row + 1
+        completion?()
+    }
+}
